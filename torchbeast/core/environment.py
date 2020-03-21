bd52801070eba6c239f8dcf3590be8e7fe92916a
pantea21 <43774709+pantea21@users.noreply.github.com>
2020-03-21 17:59:23 -0400

    Add files via upload

---
+++ torchbeast/core/environment.py
@@ -0,0 +1,90 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""The environment class for MonoBeast."""
+
+import torch
+import numpy as np
+
+
+########## The set of actions that can go as input to the DMLAB step
+DEFAULT_ACTION_SET = (
+    (0, 0, 0, 1, 0, 0, 0),    # Forward
+    (0, 0, 0, -1, 0, 0, 0),   # Backward
+    (0, 0, -1, 0, 0, 0, 0),   # Strafe Left
+    (0, 0, 1, 0, 0, 0, 0),    # Strafe Right
+    (-20, 0, 0, 0, 0, 0, 0),  # Look Left
+    (20, 0, 0, 0, 0, 0, 0),   # Look Right
+    (-20, 0, 0, 1, 0, 0, 0),  # Look Left + Forward
+    (20, 0, 0, 1, 0, 0, 0),   # Look Right + Forward
+    (0, 0, 0, 0, 1, 0, 0),    # Fire.
+)
+
+
+def _format_frame(frame):
+    frame = torch.from_numpy(frame)
+    return frame.view((1, 1) + frame.shape)  # (...) -> (T,B,...).
+
+
+class Environment:
+    def __init__(self, gym_env):
+        self.gym_env = gym_env
+        self.episode_return = None
+        self.episode_step = None
+
+    def initial(self):
+        initial_reward = torch.zeros(1, 1)
+        # This supports only single-tensor actions ATM.
+        initial_last_action = torch.zeros(1, 1, dtype=torch.int64)
+        self.episode_return = torch.zeros(1, 1)
+        self.episode_step = torch.zeros(1, 1, dtype=torch.int32)
+        initial_done = torch.ones(1, 1, dtype=torch.uint8)
+        ###### changed reset to inital to match the format of createDMLab
+        initial_frame = _format_frame(self.gym_env.initial())
+        return dict(
+            frame=initial_frame,
+            reward=initial_reward,
+            done=initial_done,
+            episode_return=self.episode_return,
+            episode_step=self.episode_step,
+            last_action=initial_last_action,
+        )
+
+    def step(self, action):
+        ######## changed actions from int to one of the default_action_set above
+        raw_action = np.array(DEFAULT_ACTION_SET[action],dtype=	np.intc)
+        frame, reward, done = self.gym_env.step(raw_action)
+        self.episode_step += 1
+        self.episode_return += reward
+        episode_step = self.episode_step
+        episode_return = self.episode_return
+        if done:
+            frame = self.gym_env.initial()
+            self.episode_return = torch.zeros(1, 1)
+            self.episode_step = torch.zeros(1, 1, dtype=torch.int32)
+
+        frame = _format_frame(frame)
+        reward = torch.tensor(reward).view(1, 1)
+        done = torch.tensor(done).view(1, 1)
+
+        return dict(
+            frame=frame,
+            reward=reward,
+            done=done,
+            episode_return=episode_return,
+            episode_step=episode_step,
+            last_action=action,
+        )
+
+    def close(self):
+        self.gym_env.close()
