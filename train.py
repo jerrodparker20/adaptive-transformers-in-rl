244c443343ee78b2d120e51f4ca6bccc8987a625
jerrodparker20 <jerrodparker20@gmail.com>
2020-03-26 18:07:17 -0600

    kj

--- train.py
+++ train.py
@@ -731,8 +731,9 @@ def train(flags):  # pylint: disable=too-many-branches, too-many-statements
                 to_log.update({k: stats.get(k, None) for k in stat_keys})
                 plogger.log(to_log)
                 # print('updating step from {} to {}'.format(step, step+(T*B)))
-                step += stats['num_unpadded_steps'] #stats.get('num_unpadded_steps', 0) #T * B
-                steps_since_sched_update += stats['num_unpadded_steps'] #.get('num_unpadded_steps', 0)
+                if len(stats) > 0:
+                    step += stats['num_unpadded_steps'] #stats.get('num_unpadded_steps', 0) #T * B
+                    steps_since_sched_update += stats['num_unpadded_steps'] #.get('num_unpadded_steps', 0)
 
         if i == 0:
             logging.info("Batch and learn: %s", timings.summary())
